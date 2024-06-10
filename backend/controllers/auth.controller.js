9817b520c96b51f0d512f54bd488510d8c1d6b91
khushaalsareen <khushaalsareen@gmail.com>
2024-06-10 21:18:01 +0530

    Sign In route created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const  User  = require("../models/user.model.js");
 const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const signup = async(req,res)=>{
     try{
@@ -33,4 +34,45 @@ const signup = async(req,res)=>{
     }
 }
 
-module.exports = {signup}
+const signin = async (req,res)=>{
+    try{
+        const {email, password} = req.body;
+
+        if(!email || !password)
+            return res.status(400).json({
+        success: false,
+        message: "Please enter all the fields"     
+    })
+
+    const validUser = await User.findOne({email})
+    if(!validUser)
+        return res.status(400).json({
+    success:false,
+    message: 'No such user exists'
+})
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if(!validPassword)
+        return res.status(400).json({
+    success: false,
+    message: 'Invalid Password'
+    })
+
+    const token = jwt.sign(
+        {id: validUser._id}, process.env.JWT_SECRET
+    )
+
+    const {password: pass, ...rest} = validUser._doc;
+
+    res.status(200).cookie('access_token',token,{
+        httpOnly: true
+    }).json(rest)
+    }
+    catch(error){
+        return res.status(500).json({
+            success: false,
+            message: 'Internal Server Error'
+        })
+    }
+}
+
+module.exports = {signup,signin}
