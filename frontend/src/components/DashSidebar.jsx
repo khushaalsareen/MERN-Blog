3d2160fd226eb867bb29d86b14ed6e6c384222f3
khushaalsareen <khushaalsareen@gmail.com>
2024-07-17 19:38:18 +0530

    added post section to dashboard

--- frontend/src/components/DashSidebar.jsx
+++ frontend/src/components/DashSidebar.jsx
@@ -11,15 +11,18 @@ import {
 import { Link, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { signoutSuccess } from '../redux/user/userSlice';
+import { useSelector } from 'react-redux';
 const DashSidebar = () => {
   const location = useLocation();
   const [tab,setTab] = useState('');
   const dispatch = useDispatch();
+  const {currentUser} = useSelector(store=>store.user)
   useEffect(()=>{
     const urlParams = new URLSearchParams(location.search);
-    // console.log(location)
+    console.log(location.search)
+    console.log(urlParams)
     const tabFromUrl = urlParams.get('tab');
-    // console.log(tabFromUrl)
+    console.log(tabFromUrl)
     if(tabFromUrl)
       setTab(tabFromUrl)
   },[location.search])
@@ -43,18 +46,29 @@ const DashSidebar = () => {
   return (
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
-            <Sidebar.ItemGroup>
+            <Sidebar.ItemGroup className='flex flex-col gap-1'>
             <Link to='/dashboard?tab=profile'>
             <Sidebar.Item
               active={tab==='profile'}
               icon={HiUser}
-              label={'User'}
+              label={currentUser.isAdmin===false?'User':'Admin'}
               labelColor='dark'
               as='div'
             >
               Profile
             </Sidebar.Item>
             </Link>
+            {currentUser.isAdmin && (
+            <Link to='/dashboard?tab=posts'>
+              <Sidebar.Item
+                active={tab === 'posts'}
+                icon={HiDocumentText}
+                as='div'
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
             <Sidebar.Item 
             icon={HiArrowSmRight}
             className='cursor-pointer' onClick={handleSignOut}
