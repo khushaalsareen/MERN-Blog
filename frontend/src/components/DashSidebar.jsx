40faeeeeae0f61cb79bd6ad9a279a41a6402be2e
khushaalsareen <khushaalsareen@gmail.com>
2024-07-18 15:30:54 +0530

    added users page to admin dashboard

--- frontend/src/components/DashSidebar.jsx
+++ frontend/src/components/DashSidebar.jsx
@@ -69,6 +69,17 @@ const DashSidebar = () => {
               </Sidebar.Item>
             </Link>
           )}
+          {currentUser.isAdmin && (
+            <Link to='/dashboard?tab=users'>
+              <Sidebar.Item
+                active={tab === 'users'}
+                icon={HiOutlineUserGroup}
+                as='div'
+              >
+                Users
+              </Sidebar.Item>
+            </Link>
+          )}
             <Sidebar.Item 
             icon={HiArrowSmRight}
             className='cursor-pointer' onClick={handleSignOut}
