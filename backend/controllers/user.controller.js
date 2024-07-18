a9588d9d4c6c4aef2828b8f76e80923b59b6941e
khushaalsareen <khushaalsareen@gmail.com>
2024-07-18 12:30:50 +0530

    created getUsers api route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -100,4 +100,48 @@ const signout = (req,res)=>{
     }
 }
 
-module.exports = {test,updateUser,deleteUser,signout}
+const getUsers = async(req,res)=>{
+    if(!req.user.isAdmin){
+        return res.status(400).json({
+            success: false,
+            message: 'You are not allowed to see all users'
+        })
+    }
+    try{
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.sort === 'asc' ? 1 : -1;
+        const users = await User.find()
+        .sort({createdAt: sortDirection})
+        .skip(startIndex)
+        .limit(limit)
+
+        const usersWithoutPassword = users.map((user)=>{
+            const {password, ...rest} = user._doc;
+            return rest;
+        })
+
+        const totalUsers = await User.countDocuments();
+        const now = new Date();
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        );
+        const lastMonthUsers  = await User.countDocuments({
+            createdAt: { $gte: oneMonthAgo}
+        })
+        return res.status(200).json({
+           users: usersWithoutPassword,
+            totalUsers,
+            lastMonthUsers
+        })
+    } catch(error){
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+module.exports = {test,updateUser,deleteUser,signout,getUsers}
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { test, updateUser, deleteUser, signout } = require('../controllers/user.controller.js');
+const { test, updateUser, deleteUser, signout, getUsers } = require('../controllers/user.controller.js');
 const { verifyToken } = require('../utils/verifyUser.js');
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.get('/test',test)
 router.put('/update/:userId',verifyToken,updateUser);
 router.delete('/delete/:userId',verifyToken,deleteUser);
 router.post('/signout',signout);
+router.get('/getusers',verifyToken,getUsers);
 
 module.exports = router
