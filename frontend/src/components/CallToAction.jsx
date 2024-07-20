da36f90279d44d7be67e69cb6515b0cbfcb589a3
khushaalsareen <khushaalsareen@gmail.com>
2024-07-20 12:52:20 +0530

    added call to action

---
+++ frontend/src/components/CallToAction.jsx
@@ -0,0 +1,11 @@
+import { Button } from 'flowbite-react';
+
+export default function CallToAction() {
+  return (
+    <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center gap-5 max-w-lg mx-auto'>
+      <div className="flex-1 justify-center flex flex-col items-center p-4">
+        <img src="https://bloggingguide.com/wp-content/uploads/2023/02/Blogging-Quotes-8.jpg" className="max-w-xs sm:max-w-sm h-auto" />
+      </div>
+    </div>
+  )
+}
--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import CallToAction from '../components/CallToAction';
 
 const PostPage = () => {
     const {postSlug} = useParams()
@@ -61,14 +62,17 @@ const PostPage = () => {
       <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
         <span className='italic'>
-          {post && (post.content.length / 100).toFixed(0)} mins read
+            {post && (post.content.length / 100).toFixed(0)
+          } mins read
         </span>
       </div>
       <div
         className='p-3 max-w-2xl mx-auto w-full post-content'
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
-     
+     <div className='max-w-4xl mx-auto w-full'>
+        <CallToAction />
+      </div>
     </main>
   )
 }
