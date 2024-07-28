f8b20861775b337045dcad6400f85ca45b389643
khushaalsareen <khushaalsareen@gmail.com>
2024-07-28 15:13:22 +0530

    added dashboard overview to admin panel

--- frontend/src/components/DashSidebar.jsx
+++ frontend/src/components/DashSidebar.jsx
@@ -47,6 +47,19 @@ const DashSidebar = () => {
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
             <Sidebar.ItemGroup className='flex flex-col gap-1'>
+            {
+              currentUser?.isAdmin && (
+                <Link to='/dashboard?tab=dash'>
+                <Sidebar.Item
+                  active={tab==='dash' || !tab}
+                  icon={HiChartPie}
+                  as='div'
+                  >
+                  Dashboard
+                </Sidebar.Item>
+                </Link>
+              )
+            }
             <Link to='/dashboard?tab=profile'>
             <Sidebar.Item
               active={tab==='profile'}
